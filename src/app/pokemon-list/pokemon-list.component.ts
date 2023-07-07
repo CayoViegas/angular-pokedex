caafce530cff34d36d2a0a67b27577453bd93920
CayoViegas <cayo.viegas@ccc.ufcg.edu.br>
2023-07-07 05:40:13 -0300

    added stylization

--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./pokemon-list.component.sass'],
 })
 export class PokemonListComponent {
-  pokemons = ['Bulbasaur', 'Ivysaur'];
+  pokemons = [
+    'Bulbasaur',
+    'Ivysaur',
+    'Venusaur',
+    'Charmander',
+    'Charmeleon',
+    'Charizard',
+  ];
 }
